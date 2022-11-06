a9ddee980fe7fd341a127a200b59387d43243739
cjh-0806 <994573025@qq.com>
2022-11-06 19:53:17 +0800

    地图导入导出

---
+++ mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "map.h"
+#include "mapwidget.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    MapWidget* m_w = new MapWidget(nullptr, map);
+    m_w->show();
+}
+
+void MainWindow::on_exportButton_clicked() //导出默认地图到指定路径
+{
+    QString dirPath = QFileDialog::getExistingDirectory(this, "选择文件夹", "/home", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    dirPath += "/default_map.txt";
+    map.map2file(dirPath.toStdString().c_str());
+    QMessageBox::information(this, "导出默认地图", "默认地图已成功保存至" + dirPath);
+}
+
+void MainWindow::on_importButton_clicked() //导入用户自己设计的地图，默认地图无错误
+{
+    QString filePath = QFileDialog::getOpenFileName(this, tr("选择文件"), "/home", "Text files (*.txt)");
+    map.file2map(filePath.toStdString().c_str());
+    QMessageBox::information(this, "导入地图", "已成功导入地图" + filePath);
+}
+
+void MainWindow::on_exitButton_clicked()
+{
+    this->close();
+}
---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QMessageBox>
+#include "map.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_startButton_clicked();
+
+    void on_exportButton_clicked();
+
+    void on_importButton_clicked();
+
+    void on_exitButton_clicked();
+
+private:
+    Map map;
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
